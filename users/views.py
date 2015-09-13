856425406542d5469a747d8c858f1619eb2ff15a
Shashi Kumar <shashiessp@gmail.com>
2015-09-13 20:43:31 +0530

    revert regisration via api

--- users/views.py
+++ users/views.py
@@ -11,9 +11,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from tastypie.exceptions import ImmediateHttpResponse
 
 
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
+from django.http import HttpResponseRedirect, HttpResponse
+
 import users.api
 
-
+"""
 def login(request):
     form_errors = {}
     if request.method == "POST":
@@ -28,10 +31,49 @@ def login(request):
     return render(request, "users/login.html",
                   {'form_errors': form_errors})
 
+"""
+
+
+def redirect_to_dashboard_if_loggedn(func):
+    def _wrapped_func(*args, **kwargs):
+        request = args[0]
+        if request.user.is_authenticated():
+            return HttpResponseRedirect('/dashboard/')
+        return func(*args, **kwargs)
+
+    return _wrapped_func
+
+
+def get_user(username_or_email):
+    try:
+        return User.objects.get(email=username_or_email)
+    except User.DoesNotExist:
+        try:
+            return User.objects.get(username=username_or_email)
+        except User.DoesNotExist:
+            return None
+
+
+@redirect_to_dashboard_if_loggedn
+def simple_login(request):
+    if request.method == 'POST':
+        username_or_email = request.POST.get('username_or_email')
+        password = request.POST.get('password')
+        user = get_user(username_or_email)
+        if user and authenticate(username=user.username, password=password):
+            user = authenticate(username=user.username, password=password)
+            django_login(request, user)
+            return HttpResponseRedirect('/dashboard/')
+        else:
+            return render(request, 'users/login.html', {'invalid': True})
+    else:
+        return render(request, 'users/login.html')    
+
 
 def logout(request):
     auth_logout(request)
     return redirect('/')
+
 
 
 def register(request):
@@ -42,8 +84,7 @@ def register(request):
             resource.post_list(request=request)
             return redirect('/dashboard/')
         except ImmediateHttpResponse, e:
-            form_errors = json.loads(e.response.content)['candidate_registration']
-
+            form_errors = json.loads(e.response.content)['registration']
+ 
     return render(request, "users/register.html",
-                  {'form_errors': form_errors})
-
+                   {'form_errors': form_errors})
